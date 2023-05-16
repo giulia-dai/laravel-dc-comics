54914e1f3aa35741996ffaf78257ba70d7b4a742
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-05-16 16:04:31 +0200

    inizio creazione colonne della table

---
+++ database/migrations/2023_05_16_135545_create_comics_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('thumb');
+            $table->string('title');
+            $table->text('description')->nullable(true);
+            $table->decimal('price', 4, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
